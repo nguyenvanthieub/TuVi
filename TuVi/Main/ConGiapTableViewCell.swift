e1e46a2ff5c1ad26697341ca06e8e2a4b2b7b977
Nguyen Van Thieu B <nguyen.van.thieub@framgias-Mac-mini-6.local>
2016-12-07 19:20:27 +0700

    develop

---
+++ TuVi/Main/ConGiapTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ConGiapTableViewCell.swift
+//  TuVi
+//
+//  Created by Nguyen Van Thieu B on 12/7/16.
+//  Copyright © 2016 Thieu Mao. All rights reserved.
+//
+
+import UIKit
+
+class ConGiapTableViewCell: UITableViewCell {
+    
+    static let identifier = "ConGiapTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
